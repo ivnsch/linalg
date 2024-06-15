c96325c8fd1215e3b11b796b84510ac35fecb90c
Ivan Schuetz <ivanschuetz@gmail.com>
2024-06-15 19:46:35 +0200

    updates to actually work on web

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,15 @@
-//! Create a custom material to draw basic lines in 3D
+//! This example demonstrates Bevy's immediate mode drawing API intended for visual debugging.
 
 mod camera_controller;
 mod rotator;
 use std::f32::consts::PI;
 
 use bevy::{
-    pbr::{MaterialPipeline, MaterialPipelineKey},
     prelude::*,
-    reflect::TypePath,
     render::{
         camera::RenderTarget,
-        mesh::{MeshVertexBufferLayout, PrimitiveTopology},
-        render_asset::RenderAssetUsages,
         render_resource::{
-            AsBindGroup, Extent3d, PolygonMode, RenderPipelineDescriptor, ShaderRef,
-            SpecializedMeshPipelineError, TextureDescriptor, TextureDimension, TextureFormat,
-            TextureUsages,
+            Extent3d, TextureDescriptor, TextureDimension, TextureFormat, TextureUsages,
         },
     },
 };
@@ -23,69 +17,74 @@ use camera_controller::{CameraController, CameraControllerPlugin};
 use rotator::{Rotator, RotatorPlugin};
 
 fn main() {
-    let mut app = App::new();
-
-    app.add_plugins((
-        DefaultPlugins,
-        MaterialPlugin::<LineMaterial>::default(),
-        CameraControllerPlugin,
-        RotatorPlugin,
-    ));
-
-    app.add_systems(
-        Startup,
-        (
-            setup_axes,
-            setup_plane,
-            setup_camera,
-            setup_light,
-            setup_x_axis,
-            setup_y_axis,
-            setup_z_axis,
-        ),
-    );
-
-    app.run();
+    App::new()
+        .add_plugins((DefaultPlugins, CameraControllerPlugin, RotatorPlugin))
+        .add_systems(
+            Startup,
+            (
+                setup_plane,
+                setup_camera,
+                setup_light,
+                setup_x_axis,
+                setup_y_axis,
+                setup_z_axis,
+            ),
+        )
+        .add_systems(Update, setup_axes)
+        .run();
 }
 
-fn setup_axes(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<LineMaterial>>,
-) {
+fn setup_axes(mut gizmos: Gizmos) {
     let size = 2.0;
-    // x axis
-    commands.spawn(MaterialMeshBundle {
-        mesh: meshes.add(LineList {
-            lines: vec![(Vec3::new(-size, 0.0, 0.0), Vec3::new(size, 0.0, 0.0))],
-        }),
-        material: materials.add(LineMaterial {
-            color: Color::GREEN,
-        }),
-        ..default()
-    });
-    // y axis
-    commands.spawn(MaterialMeshBundle {
-        mesh: meshes.add(LineList {
-            lines: vec![(Vec3::new(0.0, -size, 0.0), Vec3::new(0.0, size, 0.0))],
-        }),
-        material: materials.add(LineMaterial { color: Color::RED }),
-        ..default()
-    });
-    // z axis
-    commands.spawn(MaterialMeshBundle {
-        mesh: meshes.add(LineList {
-            lines: vec![(Vec3::new(0.0, 0.0, -size), Vec3::new(0.0, 0.0, size))],
-        }),
-        material: materials.add(LineMaterial { color: Color::BLUE }),
-        ..default()
-    });
+    let zero = 0.0;
+    // x
+    gizmos.line(
+        Vec3 {
+            x: -size,
+            y: zero,
+            z: zero,
+        },
+        Vec3 {
+            x: size,
+            y: zero,
+            z: zero,
+        },
+        Color::GREEN,
+    );
+    // y
+    gizmos.line(
+        Vec3 {
+            x: zero,
+            y: -size,
+            z: zero,
+        },
+        Vec3 {
+            x: zero,
+            y: size,
+            z: zero,
+        },
+        Color::RED,
+    );
+    // z
+    gizmos.line(
+        Vec3 {
+            x: zero,
+            y: zero,
+            z: -size,
+        },
+        Vec3 {
+            x: zero,
+            y: zero,
+            z: size,
+        },
+        Color::BLUE,
+    );
 }
 
 fn setup_camera(mut commands: Commands) {
     commands.spawn((
         Camera3dBundle {
-            transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+            transform: Transform::from_xyz(0., 1.5, 6.).looking_at(Vec3::ZERO, Vec3::Y),
             ..default()
         },
         CameraController::default(),
@@ -101,112 +100,30 @@ fn setup_light(mut commands: Commands) {
 }
 
 fn setup_plane(
-    commands: Commands,
-    meshes: ResMut<Assets<Mesh>>,
-    materials: ResMut<Assets<StandardMaterial>>,
-) {
-    let plane = Plane3d {
-        // normal: Direction3d::Z,
-        normal: Direction3d::new(Vec3 {
-            x: 1.0,
-            y: 1.0,
-            z: -1.0,
-        })
-        .unwrap(),
-    };
-    draw_plane(plane, commands, meshes, materials);
-}
-
-fn draw_plane(
-    plane: Plane3d,
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
     commands.spawn(PbrBundle {
-        mesh: meshes.add(plane.mesh().size(2.0, 2.0)),
+        mesh: meshes.add(Plane3d::default().mesh().size(2.0, 2.0)),
         material: materials.add(StandardMaterial {
             double_sided: true,
             cull_mode: None,
             base_color: Color::rgb(0.3, 0.5, 0.3),
-            ..Default::default()
+            ..default()
         }),
+        ..default()
+    });
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
         ..default()
     });
 }
 
-/// Marker component for header node
-#[derive(Debug, Clone, Component, Default, Reflect)]
-pub struct HeaderNode;
-
-/// Marker component for header text
-#[derive(Debug, Clone, Component, Default, Reflect)]
-pub struct HeaderText;
-
-#[derive(Asset, TypePath, Default, AsBindGroup, Debug, Clone)]
-struct LineMaterial {
-    #[uniform(0)]
-    color: Color,
-}
-
-impl Material for LineMaterial {
-    fn fragment_shader() -> ShaderRef {
-        "shaders/line_material.wgsl".into()
-    }
-
-    fn specialize(
-        _pipeline: &MaterialPipeline<Self>,
-        descriptor: &mut RenderPipelineDescriptor,
-        _layout: &MeshVertexBufferLayout,
-        _key: MaterialPipelineKey<Self>,
-    ) -> Result<(), SpecializedMeshPipelineError> {
-        // This is the important part to tell bevy to render this material as a line between vertices
-        descriptor.primitive.polygon_mode = PolygonMode::Line;
-        Ok(())
-    }
-}
-
-/// A list of lines with a start and end position
-#[derive(Debug, Clone)]
-struct LineList {
-    lines: Vec<(Vec3, Vec3)>,
-}
-
-impl From<LineList> for Mesh {
-    fn from(line: LineList) -> Self {
-        let vertices: Vec<_> = line.lines.into_iter().flat_map(|(a, b)| [a, b]).collect();
-
-        Mesh::new(
-            // This tells wgpu that the positions are list of lines
-            // where every pair is a start and end point
-            PrimitiveTopology::LineList,
-            RenderAssetUsages::RENDER_WORLD,
-        )
-        // Add the vertices positions as an attribute
-        .with_inserted_attribute(Mesh::ATTRIBUTE_POSITION, vertices)
-    }
-}
-
-/// A list of points that will have a line drawn between each consecutive points
-#[derive(Debug, Clone)]
-struct LineStrip {
-    points: Vec<Vec3>,
-}
-
-impl From<LineStrip> for Mesh {
-    fn from(line: LineStrip) -> Self {
-        Mesh::new(
-            // This tells wgpu that the positions are a list of points
-            // where a line will be drawn between each consecutive point
-            PrimitiveTopology::LineStrip,
-            RenderAssetUsages::RENDER_WORLD,
-        )
-        // Add the point positions as an attribute
-        .with_inserted_attribute(Mesh::ATTRIBUTE_POSITION, line.points)
-    }
-}
-
-// Marks the cube, to which the UI texture is applied.
 #[derive(Component)]
 struct Cube;
 
