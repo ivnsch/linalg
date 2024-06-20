f8247137ab092afa492578e21484ea039911c878
Ivan Schuetz <ivanschuetz@gmail.com>
2024-06-20 22:00:01 +0200

    allow to set start of vector

--- src/system_2d.rs
+++ src/system_2d.rs
@@ -26,25 +26,29 @@ fn setup_camera(mut commands: Commands) {
 
 #[derive(Component)]
 struct MyArrow {
-    target: Vec2,
+    start: Vec2,
+    end: Vec2,
 }
 
 fn draw_arrows(arrows: Query<&MyArrow>, mut gizmos: Gizmos) {
     for arrow in arrows.iter() {
-        gizmos.arrow_2d(Vec2::ZERO, arrow.target, Color::YELLOW);
+        gizmos.arrow_2d(arrow.start, arrow.end, Color::YELLOW);
     }
 }
 
-fn process_vector_str(str: &str) -> Result<Vec2, String> {
+fn process_arrow_str(str: &str) -> Result<MyArrow, String> {
     let values: Result<Vec<f32>, _> = str.split_whitespace().map(|s| s.parse::<f32>()).collect();
     match values {
-        Ok(vec) if vec.len() == 2 => {
-            let vec2 = Vec2 {
+        Ok(vec) if vec.len() == 4 => Ok(MyArrow {
+            start: Vec2 {
                 x: vec[0],
                 y: vec[1],
-            };
-            Ok(vec2)
-        }
+            },
+            end: Vec2 {
+                x: vec[2],
+                y: vec[3],
+            },
+        }),
         Ok(_) => Err("Input must contain exactly two numbers.".to_owned()),
         Err(e) => Err(format!("Failed to parse input: {}", e)),
     }
@@ -84,9 +88,9 @@ fn setup_axes(mut gizmos: Gizmos) {
 
 fn listen_inputs_from_gui(mut commands: Commands, input: Query<&mut GuiInput>) {
     for input in input.into_iter() {
-        match process_vector_str(&input.text) {
-            Ok(vec) => {
-                commands.spawn(MyArrow { target: vec });
+        match process_arrow_str(&input.text) {
+            Ok(arrow) => {
+                commands.spawn(arrow);
             }
             Err(err) => println!("error: {}", err), // TODO error handling
         }
