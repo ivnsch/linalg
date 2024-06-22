deb1064737fa6e02a0240ba98e27a5bc5987cc97
Ivan Schuetz <ivanschuetz@gmail.com>
2024-06-22 09:52:13 +0200

    refactor functions

--- src/curves_2d.rs
+++ src/curves_2d.rs
@@ -7,32 +7,15 @@ pub fn add_curves_2d_system(app: &mut App) {
 }
 
 #[allow(dead_code)]
-fn draw_square_fn(mut gizmos: Gizmos) {
-    let scaling = 20.0;
-    let x_scaling = scaling;
-    let y_scaling = scaling;
-
-    let mut last_point = None;
-
-    let half_range = 10;
-
-    for i in -half_range..half_range {
-        let x = i as f32;
-        let y = x * x;
-
-        if let Some((last_x, last_y)) = last_point {
-            gizmos.line_2d(
-                Vec2::new(last_x * x_scaling, last_y * y_scaling),
-                Vec2::new(x * x_scaling, y * y_scaling),
-                Color::WHITE,
-            );
-        }
-
-        last_point = Some((x, y));
-    }
+fn draw_square_fn(gizmos: Gizmos) {
+    draw_fn(gizmos, |x| x * x);
 }
 
-fn draw_sin_fn(mut gizmos: Gizmos) {
+fn draw_sin_fn(gizmos: Gizmos) {
+    draw_fn(gizmos, |x| x.sin());
+}
+
+fn draw_fn(mut gizmos: Gizmos, function: fn(f32) -> f32) {
     let scaling = 20.0;
     let x_scaling = scaling;
     let y_scaling = scaling;
@@ -43,7 +26,7 @@ fn draw_sin_fn(mut gizmos: Gizmos) {
 
     for i in -half_range..half_range {
         let x = i as f32;
-        let y = x.sin();
+        let y = function(x);
 
         if let Some((last_x, last_y)) = last_point {
             gizmos.line_2d(
