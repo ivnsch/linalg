98b915e2090a67819533cd30615166cf0db772ac
Ivan Schuetz <ivanschuetz@gmail.com>
2024-07-12 11:57:37 +0200

    move directly unrelated to linear algebra to new project

--- src/main.rs
+++ src/main.rs
@@ -2,42 +2,26 @@
 
 mod alg;
 mod balance_chem_eq;
-mod camera_controller;
-mod curves_2d;
-mod curves_3d;
 mod functions;
 mod grid_2d;
 mod gui;
 mod lines_2d;
-mod rotator;
-mod scratchpad_3d;
 mod system_2d;
-mod system_3d;
 mod vectors_2d_system;
-mod wave;
-mod wave_gui;
 
 use bevy::app::App;
-#[allow(unused_imports)]
-use curves_3d::add_curves_3d_system;
 use grid_2d::add_grid_2d_system;
 #[allow(unused_imports)]
 use lines_2d::add_lines_2d_system;
-#[allow(unused_imports)]
-use scratchpad_3d::add_3d_scratch;
 use system_2d::add_2d_axes;
 #[allow(unused_imports)]
 use system_2d::add_2d_space;
 #[allow(unused_imports)]
-use system_3d::add_3d_space;
-#[allow(unused_imports)]
 use vectors_2d_system::add_vectors_2d_system;
-use wave::add_wave_2d_system;
 
 fn main() {
     let app = &mut App::new();
     create_2d(app);
-    // create_3d(app);
     app.run();
 }
 
@@ -48,14 +32,5 @@ fn create_2d(app: &mut App) {
     add_grid_2d_system(app);
     // grid completely hiding axes so draw axes on top.
     add_2d_axes(app);
-    // add_lines_2d_system(app);
-    // add_curves_2d_system(app);
-    add_wave_2d_system(app);
-}
-
-#[allow(dead_code)]
-fn create_3d(app: &mut App) {
-    add_3d_space(app);
-    // add_3d_scratch(app);
-    add_curves_3d_system(app);
+    add_lines_2d_system(app);
 }
